9ea707cbeba1b09b784301b4a2a0ba0cac5d9eee
Isabel Muñoz <mariaisabel.munoz.contractor@bbva.com>
2017-11-15 09:30:06 +0100

    Added hello world from redux

--- src/App.js
+++ src/App.js
@@ -1,20 +1,52 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+
+import {createStore} from 'redux';
+
+const INITIAL_STATE = {
+  users: [],
+  loading: false,
+  error: null
+}
+
+function reducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case 'SET_USERS':
+      return {
+        ...state,
+        users: action.payload
+      }
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+store.subscribe(() => {
+  console.log(store.getState());
+})
+
+store.dispatch({
+  type: 'SET_USERS',
+  payload: [
+    {
+      id: 23,
+      name: 'Pepito',
+      age: 32
+    }, {
+      id: 11,
+      name: 'Manolín',
+      age: 20
+    }
+  ]
+});
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return (<div className="App">
+      <h1>My redux first app!</h1>
+    </div>);
   }
 }
 
