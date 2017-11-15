062edd344026a3c9548b09db363551d5ea980ef4
Isabel Muñoz <mariaisabel.munoz.contractor@bbva.com>
2017-11-15 09:43:07 +0100

    Add redux to react

--- src/App.js
+++ src/App.js
@@ -1,53 +1,20 @@
 import React, {Component} from 'react';
 import './App.css';
 
-import {createStore} from 'redux';
-
-const INITIAL_STATE = {
-  users: [],
-  loading: false,
-  error: null
-}
-
-function reducer(state = INITIAL_STATE, action) {
-  switch (action.type) {
-    case 'SET_USERS':
-      return {
-        ...state,
-        users: action.payload
-      }
-    default:
-      return state;
-  }
-}
-
-const store = createStore(reducer);
-
-store.subscribe(() => {
-  console.log(store.getState());
-})
-
-store.dispatch({
-  type: 'SET_USERS',
-  payload: [
-    {
-      id: 23,
-      name: 'Pepito',
-      age: 32
-    }, {
-      id: 11,
-      name: 'Manolín',
-      age: 20
-    }
-  ]
-});
+import {connect} from 'react-redux';
 
 class App extends Component {
   render() {
-    return (<div className="App">
-      <h1>My redux first app!</h1>
-    </div>);
+    return (
+      <div className="App">
+        <h1>My redux first app!</h1>
+        <ul>
+          {this.props.users.map((user) => <li key={user.id}>{user.name}</li>)}
+        </ul>
+      </div>);
   }
 }
 
-export default App;
+const mapStateToProps = ({users, loading}) => ({users, loading})
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -4,5 +4,56 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+
+const INITIAL_STATE = {
+  users: [],
+  loading: false,
+  error: null
+}
+
+function reducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case 'SET_USERS':
+      return {
+        ...state,
+        users: action.payload
+      }
+    case 'ADD_USER':
+      return {
+        ...state,
+        users: [
+          ...state.users,
+          action.payload
+        ]
+      }
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+store.subscribe(() => {
+  console.log(store.getState());
+})
+
+store.dispatch({
+  type: 'SET_USERS',
+  payload: [
+    {
+      id: 23,
+      name: 'Pepito',
+      age: 32
+    }, {
+      id: 11,
+      name: 'Manolín',
+      age: 20
+    }
+  ]
+});
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
