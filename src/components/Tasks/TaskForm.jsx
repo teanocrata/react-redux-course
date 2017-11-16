939f1dd3f831a746a543fa9ac250ef78c8cff18f
Isabel Muñoz <mariaisabel.munoz.contractor@bbva.com>
2017-11-16 10:01:10 +0100

    Add form feature

--- src/components/Tasks/TaskForm.jsx
+++ src/components/Tasks/TaskForm.jsx
@@ -1,6 +1,58 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
-const TaskForm = () => (<div>Task Form</div>)
+import {connect} from 'react-redux';
+import {addTask} from '../../redux/tasks/actions';
 
-export default TaskForm
+class TaskForm extends Component {
+  constructor(props) {
+    super(props);
+    this.formNodes = {};
+  }
+
+  refFn = (node) => {
+    this.formNodes[node.name] = node;
+  }
+
+  clearForm() {
+    Object.keys(this.formNodes).forEach(nodeName => this.formNodes[nodeName].value = '');
+  }
+
+  formSubmitHandler = (event) => {
+    event.preventDefault();
+    this.props.addTask({
+      name: this.formNodes.name.value,
+      priority: + this.formNodes.priority.value,
+      owner: this.formNodes.owner.value
+    })
+    this.clearForm();
+  }
+  render() {
+    return (<form onSubmit={this.formSubmitHandler}>
+      <fieldset>
+        <legend>Create a task</legend>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input ref={this.refFn} id="name" name="name" type="text"></input>
+          <div>
+            <label>Priority:</label>
+            <input ref={this.refFn} id="priority" name="priority" type="number"></input>
+            <label>Owner:</label>
+            <input ref={this.refFn} id="owner" name="owner" type="text"></input>
+          </div>
+        </div>
+        <button>Create task</button>
+      </fieldset>
+    </form>);
+  }
+};
+
+TaskForm.propTypes = {
+  addTask: PropTypes.func.isRequired
+};
+
+const mapDispatchToProps = {
+  addTask
+};
+
+export default connect(null, mapDispatchToProps)(TaskForm);
--- src/redux/tasks/actions.js
+++ src/redux/tasks/actions.js
@@ -3,5 +3,17 @@ import {
   REMOVE_TASK}
 from './constants'
 
-export const addTask = (task) => ({type: ADD_TASK, payload: task});
+let id = 0;
+
+// const generatedId = ((first) => () => first++)(0)
+
+export const addTask = (task) => {
+  return {
+    type: ADD_TASK,
+    payload: {
+      id: id++,
+      ...task
+    }
+  }
+};
 export const removeTask = (taskId) => ({type: REMOVE_TASK, payload: taskId});
