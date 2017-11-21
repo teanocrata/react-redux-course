e12ac140db76f145078533ba8a7f0a36ebe8550d
Isabel Muñoz <mariaisabel.munoz.contractor@bbva.com>
2017-11-21 11:14:43 +0100

    Close modal on esc

--- src/components/ui/Modal/index.jsx
+++ src/components/ui/Modal/index.jsx
@@ -4,25 +4,45 @@ import PropTypes from 'prop-types';
 import RemoveTaskDialog from './RemoveTaskDialog';
 
 import {connect} from 'react-redux';
-import { getModal } from 'redux/modal/reducer';
-import { closeModal } from 'redux/modal/actions';
+import {getModal} from 'redux/modal/reducer';
+import {closeModal} from 'redux/modal/actions';
+
+import keycode  from 'keycode';
 
 const MODAL_COMPONENTS = {
   RemoveTaskDialog
 };
 
 class Modal extends Component {
+  closeModal = (event) => {
+    if (keycode(event) === 'esc') {
+      this.props.closeModal();
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.child === null && nextProps.child) {
+      document.addEventListener('keydown', this.closeModal)
+    }
+    if (this.props.child && nextProps.child === null) {
+      document.removeEventListener('keydown', this.closeModal)
+    }
+  }
   render() {
     const {child, childProps} = this.props;
     const ModalChild = MODAL_COMPONENTS[this.props.child] || null;
     return (
-      child ? <section style={{display: 'block'}} className="w3-modal">
-      <div className="w3-modal-content">
-        <button onClick={this.props.closeModal} className="w3-btn w3-display-topright">x</button>
-        {<ModalChild {...childProps}/>}
-      </div>
+      child
+      ? <section style={{
+          display: 'block'
+        }} className="w3-modal">
+        <div className="w3-modal-content">
+          <button onClick={this.props.closeModal} className="w3-btn w3-display-topright">x</button>
+          {<ModalChild {...childProps}/>}
+        </div>
 
-    </section> : null)
+      </section>
+      : null)
   }
 }
 
@@ -36,6 +56,8 @@ const mapStateToProps = state => ({
   ...getModal(state)
 });
 
-const mapDispatchToProps = { closeModal };
+const mapDispatchToProps = {
+  closeModal
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Modal);
